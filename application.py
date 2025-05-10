f4b7697581ba4553ff81411d1a2ecd4633a821cf
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-05-10 18:42:52 +0330

    add drink class and get_dinks Function

--- application.py
+++ application.py
@@ -1,9 +1,28 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80),unique=True,nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description} "
 
 @app.route('/')
 def home():
     return 'Hello, Flask!'
+
+
+
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+    return {"drinks": drinks}
 
 
