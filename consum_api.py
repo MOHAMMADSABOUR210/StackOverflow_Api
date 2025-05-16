0f0c0032ae378eb2fe8aa9216425029a28538cf3
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-05-16 12:54:22 +0330

    savinNow the storage section is optional.

--- consum_api.py
+++ consum_api.py
@@ -14,6 +14,9 @@ BASE_URL = "https://api.stackexchange.com/2.3"
 @app.route('/search')
 def search_question():
     query = request.args.get('query')
+    save_csv = request.args.get('save', 'false').lower() == 'true'
+
+
     url = f"{BASE_URL}/search?order=desc&sort=activity&intitle={query}&site=stackoverflow"
     res = requests.get(url)
     data = res.json()
@@ -26,10 +29,12 @@ def search_question():
             'is_answered': item['is_answered']
         })
 
-    csv_file = 'questions.csv'
-    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=['title', 'link', 'is_answered'])
-        writer.writeheader()
-        writer.writerows(questions)
+        
+    if save_csv :
+        csv_file = 'questions.csv'
+        with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['title', 'link', 'is_answered'])
+            writer.writeheader()
+            writer.writerows(questions)
 
     return jsonify({'message': 'CSV file created', 'file': csv_file, 'total_questions': len(questions)})
