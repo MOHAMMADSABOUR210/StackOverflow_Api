4c8f5ef5448cc4d57893d3d82434fa286ec88ad2
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-05-16 11:32:26 +0330

    create add_drink function to add new drinks

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
@@ -41,3 +41,11 @@ def get_drink(id):
     drink = Drink.query.get_or_404(id)
     return {'name': drink.name,
                       'description' : drink.description}
+
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'], description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
